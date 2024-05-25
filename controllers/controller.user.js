23f0cedbe3369679ba8fb3df731e20eec5f7ccda
Kminnureddy <konthamminnureddy@gmail.com>
2024-05-25 16:11:00 +0530

    delete completed

--- controllers/controller.user.js
+++ controllers/controller.user.js
@@ -12,12 +12,38 @@ router.get('/',async(req,res)=>{
     res.send(users.rows)
 })
 //post request
-router.post('/createuser',async(req,res)=>{
+router.post('/createUser',async(req,res)=>{
 
     const data =req.body
-
-   // console.log(data);
+    console.log(data);
     const user =await services.createUser(data)
     res.json({message:"user added succesfully"})
+
+
+     
+}) 
+router.get("/user/:name/", async(request, response)=> {
+    const name = request.params.name
+    const user = await services.getUser(name)
+    response.send(user.rows)
+
 })
+
+router.put("/updateuser/:name", async(request, response) => {
+    const name = request.params.name
+    const data = request.body
+    const updateduser = await services.updateUser(name, data)
+    response.json({message:"User is updated"})
+})
+
+
+router.delete("/deleteuser/:name", async(request, response) => {
+    const name = request.params.name
+    // const data = request.body
+    const deleteduser = await services.deleteUser(name)
+
+    response.json({message:`${name} is Deleted successfully`})
+})
+
+
 module.exports = router
--- services/services.user.js
+++ services/services.user.js
@@ -11,3 +11,18 @@ module.exports.createUser =async(data)=>{
     const user =await db.query('INSERT INTO public.crudoperations(name,age,profession) VALUES ($1,$2,$3)',[data.name,data.age,data.profession])
     return user
 }
+module.exports.getUser = async(name) => {
+    const user = await db.query ("SELECT * FROM public.crudoperations WHERE name = $1", [name])
+    return user
+}
+
+module.exports.updateUser = async(name,data) => {
+    const user = await db.query("UPDATE public.crudoperations SET name = $1, age = $2, profession = $3 WHERE name = $4", [data.name, data.age, data.profession, name])
+    return user
+}
+
+module.exports.deleteUser = async(name) => {
+    const user = await db.query("DELETE FROM public.crudoperations WHERE name = $1",[name])
+
+    return user
+}
